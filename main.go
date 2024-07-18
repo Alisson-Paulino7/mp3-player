e793219745558130303a65faa0b2a7079aeb10aa
alisson <alissonpaulino007@gmail.com>
2024-07-18 08:31:10 -0300

    correo de dependências

--- main.go
+++ main.go
@@ -4,69 +4,68 @@ import (
 	"io"
 	"os"
 
+	"fyne.io/fyne"
 	"fyne.io/fyne/app"
-	"fyne.io/fyne/v2"
-	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/dialog"
-	"fyne.io/fyne/v2/widget"
+	"fyne.io/fyne/container"
+	"fyne.io/fyne/dialog"
+	"fyne.io/fyne/widget"
 	"github.com/hajimehoshi/oto"
 	"github.com/tosone/minimp3"
 )
 
-
 func main() {
-    myApp := app.New()
-    myWindow := myApp.NewWindow("Tocador MP3")
+	myApp := app.New()
+	myWindow := myApp.NewWindow("Tocador MP3")
 
-    var player *oto.Player
-    var decoder *minimp3.Decoder
-    var context *oto.Context
+	var player *oto.Player
+	var decoder *minimp3.Decoder
+	var context *oto.Context
 
-    playButton := widget.NewButton("Tocar", func() {
-        if decoder != nil {
-            go func() {
-                buffer := make([]byte, 8192)
-                for {
-                    n, err := decoder.Read(buffer)
-                    if err == io.EOF {
-                        break
-                    }
-                    if err != nil {
-                        break
-                    }
-                    player.Write(buffer[:n])
-                }
-            }()
-        }
-    })
+	playButton := widget.NewButton("Tocar", func() {
+		if decoder != nil {
+			go func() {
+				buffer := make([]byte, 8192)
+				for {
+					n, err := decoder.Read(buffer)
+					if err == io.EOF {
+						break
+					}
+					if err != nil {
+						break
+					}
+					player.Write(buffer[:n])
+				}
+			}()
+		}
+	})
 
-    openButton := widget.NewButton("Abrir", func() {
-        dialog.NewFileOpen(func(read fyne.URIReadCloser, err error) {
-            if err == nil && read != nil {
-                if player != nil {
-                    player.Close()
-                }
-                if decoder != nil {
-                    decoder.Close()
-                }
-                if context != nil {
-                    context.Close()
-                }
+	openButton := widget.NewButton("Abrir", func() {
+		dialog.NewFileOpen(func(read fyne.URIReadCloser, err error) {
+			if err == nil && read != nil {
+				if player != nil {
+					player.Close()
+				}
+				if decoder != nil {
+					decoder.Close()
+				}
+				if context != nil {
+					context.Close()
+				}
 
-                file, _ := os.Open(read.URI().Path())
-                decoder, _ = minimp3.NewDecoder(file)
-                context, _ = oto.NewContext(decoder.SampleRate, decoder.Channels, 2, 8192)
-                player = context.NewPlayer()
-            }
-        }, myWindow).Show()
-    })
+				file, _ := os.Open(read.URI().Path())
+				decoder, _ = minimp3.NewDecoder(file)
+				context, _ = oto.NewContext(decoder.SampleRate, decoder.Channels, 2, 8192)
+				player = context.NewPlayer()
+			}
+		}, myWindow).Show()
+	})
 
-    content := container.NewVBox(
-        openButton,
-        playButton,
-    )
+	content := container.NewVBox(
+		openButton,
+		playButton,
+	)
 
-    myWindow.SetContent(content)
-    myWindow.Resize(fyne.NewSize(500, 900))
-    myWindow.ShowAndRun()
+	myWindow.SetContent(content)
+	myWindow.Resize(fyne.NewSize(500, 900))
+	myWindow.ShowAndRun()
 }
